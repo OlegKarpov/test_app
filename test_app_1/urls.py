f7e24fecb9357c3958e05a667d6f22484dcacfdc
o.karpov <o.karpov@sanoma.com>
2019-07-30 23:03:17 +0300

    login page redirect

--- test_app_1/urls.py
+++ test_app_1/urls.py
@@ -13,18 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # url(r'^accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', login_required(TemplateView.as_view(template_name='index.html')), name='index'),
 ]
 
 if settings.DEBUG:
